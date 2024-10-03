97115942b2f89b2698be741d938ca08e9f5a1816
Pierre SCHWEITZER <pierre.schweitzer@epita.fr>
2024-10-03 15:49:44 +0200

    fix(gatling): adding type to value inside scenarios file

--- src/test/scala/example/ExampleSimulation.scala
+++ src/test/scala/example/ExampleSimulation.scala
@@ -1,22 +1,24 @@
 package example
 
 import io.gatling.core.Predef._
+import io.gatling.core.structure.{ChainBuilder, ScenarioBuilder}
 import io.gatling.http.Predef._
+import io.gatling.http.protocol.HttpProtocolBuilder
 
 import java.util.concurrent.ThreadLocalRandom
 
 class ExampleSimulation extends Simulation {
 
-  val checkPrice = exec(
+  val checkPrice: ChainBuilder = exec(
     http("Get Price")
       .get("/v1/bpi/currentprice.json")
       .check(status.is(200))
   )
 
-  val httpProtocol =
+  val httpProtocol: HttpProtocolBuilder =
     http.baseUrl("https://api.coindesk.com")
 
-  val bitcoin = scenario("Bitcoin price check").exec(checkPrice)
+  val bitcoin: ScenarioBuilder = scenario("Bitcoin price check").exec(checkPrice)
 
   setUp(
     bitcoin.inject(rampUsers(10).during(10))
